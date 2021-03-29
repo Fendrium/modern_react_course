17b8deeafba0edb4b1284b9a2ac891f74f52b151
Charlie NVP <charlie.nguyenvanphuc@gmail.com>
2021-03-29 15:31:18 +0900

    Remove some direct information

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyBddng2WeUh0Z6l7TxBAjkgbYfn50XUHvM';
+const KEY = 'API_KEY';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
