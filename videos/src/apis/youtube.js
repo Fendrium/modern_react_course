e12917e7ccc509146d0c9c0a371c26c3b02d8782
Charlie NVP <charlie.nguyenvanphuc@gmail.com>
2021-04-05 16:37:48 +0900

    Remove key info

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
 const KEY = 'API_KEY';
-=======
-const KEY = 'AIzaSyBddng2WeUh0Z6l7TxBAjkgbYfn50XUHvM';
->>>>>>> d2cfc29fd2db91f7c8de106bc9aef45ccbc2da1c
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
